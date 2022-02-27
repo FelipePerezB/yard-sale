aac10b6f1600d70fd4a6c0da1384426967c2b16b
felipe <felipeeperez3@gmail.com>
2022-02-27 20:22:59 -0300

    se arregla el add icon

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -4,16 +4,10 @@ import "@styles/AddToCart.css"
 import add from "@icons/add-to-shopping-cart.svg"
 import added from "@icons/added.svg"
 
-function sum(n){
-  return n+1
-}
-
 let n = 0
-
 let id=0
 
 function AddToCart() {
-  const addedElement = <img className='added-image' src={added}/>
   const [icon, addedToTheCart] = useState(false)
   const handleToggle = () =>{
     (icon) ? n-=1 : n+=1
@@ -26,8 +20,11 @@ function AddToCart() {
     <div className='add-button'>
       <label htmlFor={"add-to-cart-"+id}>{""}</label>
         <input id={"add-to-cart-"+id} type="checkbox" onClick={handleToggle}/>
-      {icon && addedElement}
-      <img alt='add to the shopping cart' className='add-image' src={add}/>
+      <img 
+        alt='add to the shopping cart'
+        className='add-image'
+        src={icon ? added : add}
+      />
     </div>   
   )
 }
