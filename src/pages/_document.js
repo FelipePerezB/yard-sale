8fe25f3feece70925ffe42fb70ddc62d61ede15b
felipe <felipeeperez3@gmail.com>
2022-03-21 09:43:56 -0300

    Se agrega la font

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,9 @@ class MyDocument extends Document{
     return(
       <Html lang="en">
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+          <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;700&display=swap" rel="stylesheet"/>
           <title>Yard sale</title>
         </Head>
         <body>
